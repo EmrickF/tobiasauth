9fecf21cb696f8fbb37d8222a2edf8d365c4ee6f
EmrickF <erik.finnberg07@gmail.com>
2025-09-23 13:41:56 +0300

    awddawd

---
+++ src/app/pages/login.tsx
@@ -0,0 +1,35 @@
+"use client";
+import React from 'react'
+
+export default function Page() {
+
+  return (
+    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Logga in på ditt konto</h2>
+      </div>
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6">
+          <div>
+            <label htmlFor="email" className="block text-sm/6 font-medium text-gray-100">E-postadress</label>
+            <div className="mt-2">
+              <input id="email" type="email" name="email" required autoComplete="email" className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+            </div>
+          </div>
+          <div>
+            <div className="flex items-center justify-between">
+              <label htmlFor="password" className="block text-sm/6 font-medium text-gray-100">Lösenord</label>
+            </div>
+            <div className="mt-2">
+              <input id="password" type="password" name="password" required autoComplete="current-password" className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+            </div>
+          </div>
+          <div>
+            <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Logga in</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
